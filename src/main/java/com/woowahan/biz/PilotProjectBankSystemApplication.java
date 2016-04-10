918c2710125b0ae8cdc8a0bf60f6e6fb3b4f18e9
philo-grammer <great1106@gmail.com>
2016-04-10 17:05:55 +0900

    BIZPILOT-74 logback 설정

--- src/main/java/com/woowahan/biz/PilotProjectBankSystemApplication.java
+++ src/main/java/com/woowahan/biz/PilotProjectBankSystemApplication.java
@@ -1,12 +1,19 @@
 package com.woowahan.biz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PilotProjectBankSystemApplication {
 
+    private static Logger logger =
+            LoggerFactory.getLogger(PilotProjectBankSystemApplication.class);
+
     public static void main(String[] args) {
+        logger.info("=========Starting application========");
         SpringApplication.run(PilotProjectBankSystemApplication.class, args);
+        logger.warn("=========Ending application=========");
     }
 }
