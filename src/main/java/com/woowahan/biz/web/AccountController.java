df400fb705e4b114ece567b5876aad29f4919696
philo-grammer <great1106@gmail.com>
2016-04-10 19:55:06 +0900

    BIZPILOT-37 계좌등록 컨트롤러 구현
    - logback 사용 (System.out.println()과 비교)

--- src/main/java/com/woowahan/biz/web/AccountController.java
+++ src/main/java/com/woowahan/biz/web/AccountController.java
@@ -2,6 +2,8 @@ package com.woowahan.biz.web;
 
 import com.woowahan.biz.domain.Account;
 import com.woowahan.biz.service.AccountService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +25,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RequestMapping("accounts")
 public class AccountController {
 
+    public static final Logger logger =
+            LoggerFactory.getLogger(AccountController.class);
+
     @Autowired
     AccountService accountService;
 
@@ -38,6 +43,16 @@ public class AccountController {
     public Account getAccount(@PathVariable("accountId") Long accountId) {
         Account account = accountService.findAccount(accountId);
         return account;
+    }
+
+    /** 계좌등록 */
+    @RequestMapping(method = POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public String register(@RequestBody Account account) {
+        accountService.registerAccount(account);
+        System.out.println("account = " + account);
+        logger.info("account = {}", account);
+        return "OK";
     }
 
 }
