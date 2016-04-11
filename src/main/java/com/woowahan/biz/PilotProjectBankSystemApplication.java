726b8a0e3782230862119da70a56191f8159a63c
philo-grammer <great1106@gmail.com>
2016-04-11 22:04:15 +0900

    @Slf4j 어노테이션 사용하는 것으로 변경

--- src/main/java/com/woowahan/biz/PilotProjectBankSystemApplication.java
+++ src/main/java/com/woowahan/biz/PilotProjectBankSystemApplication.java
@@ -1,19 +1,16 @@
 package com.woowahan.biz;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class PilotProjectBankSystemApplication {
 
-    private static Logger logger =
-            LoggerFactory.getLogger(PilotProjectBankSystemApplication.class);
-
     public static void main(String[] args) {
-        logger.info("=========Starting application========");
+        log.info("=========Starting application========");
         SpringApplication.run(PilotProjectBankSystemApplication.class, args);
-        logger.warn("=========Ending application=========");
+        log.warn("=========Ending application=========");
     }
 }
